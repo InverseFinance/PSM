979ecdf0d5033f0c0d64001c7a44d72262f50f65
0xtj24 <tabboz23@proton.me>
2025-12-04 10:55:20 -0500

    rm psm address in Contraction and Expansion events

--- src/PSMFed.sol
+++ src/PSMFed.sol
@@ -22,8 +22,8 @@ contract PSMFed {
     event PendingGovUpdated(address indexed pendingGov);
     event ChairChanged(address indexed oldChair, address indexed newChair);
     event SupplyCapUpdated(uint256 oldSupplyCap, uint256 newSupplyCap);
-    event Expansion(address indexed psm, uint256 amount);
-    event Contraction(address indexed psm, uint256 amount);
+    event Expansion(uint256 amount);
+    event Contraction(uint256 amount);
 
     constructor(address _psm, address _gov, address _chair, address _dola) {
         psm = _psm;
@@ -54,7 +54,7 @@ contract PSMFed {
         require(amount + supply <= supplyCap, "Supply cap exceeded");
         supply += amount;
         DOLA.mint(psm, amount);
-        emit Expansion(psm, amount);
+        emit Expansion(amount);
     }
 
     /**
@@ -69,7 +69,7 @@ contract PSMFed {
         supply -= amount;
         DOLA.transferFrom(psm, address(this), amount);
         DOLA.burn(amount);
-        emit Contraction(psm, amount);
+        emit Contraction(amount);
     }
 
     /**
